7154a9e8433493a31b2d2061893a08a7633b7b50
mostafa <mostafa.mohamed.azaz1234@gmail.com>
2022-09-25 12:19:06 +0200

    update all

--- resources/views/web/front/layout/header/nav.blade.php
+++ resources/views/web/front/layout/header/nav.blade.php
@@ -5,7 +5,7 @@ use App\Models\ChangeConstant;
 <nav  class="navbar navbar-expand-lg bg-light sticky-top ">
     <div class="container">
         @include('web.front.layout.header.logo')
-        <a href="#form"  class="btn ms-auto main-btn hidden_mobile" type="submit">
+        <a href="#formview"  class="btn ms-auto main-btn hidden_mobile" type="submit">
            @if(isset($changeconstant->enrollnow)) {{$changeconstant->enrollnow}} @endif
         </a>
     </div>
