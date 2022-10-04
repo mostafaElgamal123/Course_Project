f3b530cb86359c20641b59070e1a50c1c70f3415
mostafa <mostafa.mohamed.azaz1234@gmail.com>
2022-10-04 14:26:17 +0200

    update all

--- resources/views/web/front/single/single.blade.php
+++ resources/views/web/front/single/single.blade.php
@@ -1,8 +1,4 @@
-
-
 @extends('web.front.master')
-
-
 @section('title','Course Detail')
 @section('content')
     @include('web.front.vendors.single.course')
--- resources/views/web/front/success/success.blade.php
+++ resources/views/web/front/success/success.blade.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="ar">
 @include('web.front.layout.head')
-@section('ScriptVendors')
-<!-- <script>
-   fbq('track', 'Lead')
-   fbq('track', 'CompleteRegistration');
-</script> -->
-@endsection
-@section('title','success')
+@section('scriptcompleteregistration')
+    <!-- <script>
+        fbq('track', 'Lead');
+        fbq('track', 'CompleteRegistration');
+    </script> -->
+@show
 <body dir="rtl">
 @include('web.front.vendors.success.success')
 </body>
